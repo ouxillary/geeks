73d0718205638e84f6f118c0d8a0271956417ccd
root <root@localhost.localdomain>
2017-01-27 08:45:41 +0700

    Hell World

---
+++ wifi_repair.sh
@@ -0,0 +1,142 @@
+{
+   "app_list": {
+      "app_launch_count": 0,
+      "how_enabled": 5,
+      "last_app_launch_ping": "13129948800000000",
+      "last_launch_ping": "13129948800000000",
+      "launch_count": 0,
+      "profile": "Default",
+      "when_enabled": "0"
+   },
+   "apps": {
+      "app_launcher": {
+         "has_been_enabled": true
+      }
+   },
+   "background_mode": {
+      "enabled": false
+   },
+   "browser": {
+      "enabled_labs_experiments": [  ],
+      "last_redirect_origin": ""
+   },
+   "data_reduction": {
+      "daily_original_length": [ "31734873", "0", "0", "0", "0", "0", "0", "0", "19679184", "2895915", "68659134", "220683714", "108339559", "47747138", "58911089", "0", "5150408", "579170", "0", "0", "445375546", "969206628", "2395707", "334", "0", "0", "0", "79185", "0", "43180343", "3669397", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "3756855", "0", "309769080", "0", "33652254", "8608360", "9203", "5821", "106701539", "108147320" ],
+      "daily_original_length_via_data_reduction_proxy": [ "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" ],
+      "daily_original_length_with_data_reduction_proxy_enabled": [ "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" ],
+      "daily_received_length": [ "31734873", "0", "0", "0", "0", "0", "0", "0", "19679184", "2895915", "68659134", "220683714", "108339559", "47747138", "58911089", "0", "5150408", "579170", "0", "0", "445375546", "969206628", "2395707", "334", "0", "0", "0", "79185", "0", "43180343", "3669397", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "3756855", "0", "309769080", "0", "33652254", "8608360", "9203", "5821", "106701539", "108147320" ],
+      "daily_received_length_https_with_data_reduction_proxy_enabled": [ "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" ],
+      "daily_received_length_long_bypass_with_data_reduction_proxy_enabled": [ "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" ],
+      "daily_received_length_short_bypass_with_data_reduction_proxy_enabled": [ "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" ],
+      "daily_received_length_unknown_with_data_reduction_proxy_enabled": [ "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" ],
+      "daily_received_length_via_data_reduction_proxy": [ "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" ],
+      "daily_received_length_with_data_reduction_proxy_enabled": [ "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" ],
+      "last_update_date": "13129923600000000"
+   },
+   "geolocation": {
+      "access_token": {
+
+      }
+   },
+   "gl_renderer_string": "Mesa DRI Intel(R) 945GM x86/MMX/SSE2",
+   "gl_vendor_string": "Tungsten Graphics, Inc",
+   "gl_version_string": "1.4 Mesa 8.0.5",
+   "hardware_acceleration_mode_previous": true,
+   "http_original_content_length": "11672935359",
+   "http_received_content_length": "11672935359",
+   "local_state": {
+      "multiple_profile_prefs_version": 7
+   },
+   "ntp": {
+      "promo_resource_cache_update": "1485476085.747968"
+   },
+   "policy": {
+      "last_statistics_update": "13129907567143423"
+   },
+   "profile": {
+      "info_cache": {
+         "Default": {
+            "active_time": 1485480912.697666,
+            "avatar_icon": "chrome://theme/IDR_PROFILE_AVATAR_0",
+            "background_apps": true,
+            "is_ephemeral": false,
+            "is_omitted_from_profile_list": false,
+            "is_using_default_name": true,
+            "managed_user_id": "",
+            "name": "First user",
+            "user_name": ""
+         }
+      },
+      "last_active_profiles": [ "Default" ],
+      "last_used": "Default"
+   },
+   "protocol_handler": {
+      "excluded_schemes": {
+         "afp": true,
+         "data": true,
+         "disk": true,
+         "disks": true,
+         "file": true,
+         "hcp": true,
+         "javascript": true,
+         "mailto": false,
+         "ms-help": true,
+         "news": false,
+         "nntp": true,
+         "shell": true,
+         "snews": false,
+         "vbscript": true,
+         "view-source": true,
+         "vnd": {
+            "ms": {
+               "radio": true
+            }
+         }
+      }
+   },
+   "show-first-run-bubble-option": 0,
+   "shutdown": {
+      "num_processes": 0,
+      "num_processes_slow": 0,
+      "type": 0
+   },
+   "task_manager": {
+      "window_placement": {
+         "bottom": 560,
+         "left": 413,
+         "maximized": false,
+         "right": 873,
+         "top": 290,
+         "work_area_bottom": 780,
+         "work_area_left": 0,
+         "work_area_right": 1280,
+         "work_area_top": 29
+      }
+   },
+   "uninstall_metrics": {
+      "installation_date2": "1439733098",
+      "launch_count": "501"
+   },
+   "user_experience_metrics": {
+      "low_entropy_source2": 3218,
+      "session_id": 500,
+      "stability": {
+         "breakpad_registration_fail": 500,
+         "child_process_crash_count": 6,
+         "crash_count": 153,
+         "debugger_not_present": 500,
+         "execution_phase": 500,
+         "exited_cleanly": false,
+         "last_timestamp_sec": "1485481209",
+         "launch_count": 501,
+         "launch_time_sec": "1485480309",
+         "plugin_stats2": [  ],
+         "session_end_completed": true,
+         "stats_buildtime": "1411879878",
+         "stats_version": "37.0.2062.120-devel"
+      }
+   },
+   "was": {
+      "restarted": false
+   }
+}
